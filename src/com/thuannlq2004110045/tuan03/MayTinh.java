d4e078a3f5b8d4470091f4aaf67bc018ae442540
Thuannlq2004110045 <quangthuan672@gmail.com>
2021-09-26 18:39:43 -0800

    bài tập tuần 3

---
+++ src/com/thuannlq2004110045/tuan03/MayTinh.java
@@ -0,0 +1,28 @@
+package com.thuannlq2004110045.tuan03;
+
+public class MayTinh {
+    String nhaSanXuat;
+    String namSanXuat;
+    String heDieuHanh;
+    String ram;
+    String cpu;
+    double gia;
+    String namBaoHanh;
+
+    MayTinh(){
+
+    }
+
+    MayTinh(String nSX, String naSX, String hDH){
+        nhaSanXuat = nSX;
+        namSanXuat = naSX;
+        heDieuHanh = hDH;
+    }
+
+    MayTinh(String r, String cp, double g, String nBH){
+        ram = r;
+        cpu = cp;
+        gia = g;
+        namBaoHanh = nBH;
+    }
+}
---
+++ src/com/thuannlq2004110045/tuan03/TaiKhoan.java
@@ -0,0 +1,17 @@
+package com.thuannlq2004110045.tuan03;
+
+public class TaiKhoan {
+    String tenChuTaiKhoan;
+    String soTaiKhoan;
+    int soDuTaiKhoan;
+
+    TaiKhoan(){
+
+    }
+
+    TaiKhoan(String teCTK,String sTK, int sdTK){
+        tenChuTaiKhoan = teCTK;
+        soTaiKhoan = sTK;
+        soDuTaiKhoan = sdTK;
+    }
+}
