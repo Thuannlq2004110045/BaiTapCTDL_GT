17597577579929e6422b79d271bd505e3b1990df
Thuannlq2004110045 <quangthuan672@gmail.com>
2021-10-04 07:37:34 -0800

    nộp bài tuần 2

---
+++ src/com/thuannlq2004110045/tuan02/Xe.java
@@ -0,0 +1,24 @@
+package com.thuannlq2004110045.tuan02;
+
+public class Xe {
+    String tenChuXe;
+    String hangSanXuat;
+    String dong;
+    String giayPhep;
+    String dungTichXang;
+
+    Xe(){
+
+    }
+
+    Xe(String teCX, String haSX, String dg){
+        tenChuXe = teCX;
+        hangSanXuat = haSX;
+        dong = dg;
+    }
+
+    Xe(String gP, String dTX){
+        giayPhep = gP;
+        dungTichXang = dTX;
+    }
+}
